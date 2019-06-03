cd734abfdfecfd568560512362987abe0110f617
talicopanda <tales.scopinho@gmail.com>
2019-06-03 14:11:19 -0400

    Code to test

--- FunctionRide/src/functionride/Level.java
+++ FunctionRide/src/functionride/Level.java
@@ -3,82 +3,319 @@
  * May 28th 2019 
  * template for a level type object
  */
-package functionride; 
+package functionride;
+
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.io.*;
+
 public class Level {
+
     //attributes 
     private Point startPoint;
     private Point endPoint;
-    private AbstractObstacle[] obstacles; 
-    
+    private AbstractObstacle[] obstacles;
 
     /**
-     * constructor for a new level with all attributes 
+     * constructor for a new level with all attributes
+     *
      * @param startPoint the position of the starting platform
-     * @param endPoint the position of the ending platform 
-     * @param obstacles an array of all obstacles in the level 
+     * @param endPoint the position of the ending platform
+     * @param obstacles an array of all obstacles in the level
      */
-    public Level (Point startPoint, Point endPoint, AbstractObstacle[] obstacles) { 
+    public Level(Point startPoint, Point endPoint, AbstractObstacle[] obstacles) {
         this.startPoint = startPoint;
         this.endPoint = endPoint;
-        this.obstacles = obstacles; 
+        this.obstacles = obstacles;
     }
-    
-    public void setStartPoint(Point startPoint) { 
-        this.startPoint = startPoint; 
+
+    public void setStartPoint(Point startPoint) {
+        this.startPoint = startPoint;
     }
-    
-    public Point getStartPoint() { 
+
+    public Point getStartPoint() {
         return startPoint;
     }
-     public void setEndPoint(Point endPoint) { 
-        this.endPoint = endPoint; 
+
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
     }
-    
-    public Point getendPoint() { 
+
+    public Point getendPoint() {
         return endPoint;
     }
+
     /**
      * draws the starting platform
+     *
      * @param g the drawing utensil
      */
-    public void drawStart(Graphics2D g) { 
-        g.drawRect((int)startPoint.getX(),(int)startPoint.getY(), 30, 10);
+    public void drawStart(Graphics2D g2d) {
+        g2d.drawOval((int) startPoint.getX(), (int) startPoint.getY(), 10, 10);
     }
-     /**
+
+    /**
      * draws the ending platform
-     * @param g the drawing utensil 
+     *
+     * @param g the drawing utensil
      */
-    public void drawEnd(Graphics2D g) { 
-        g.drawRect((int)endPoint.getX(),(int)endPoint.getY(), 30, 10);
+    public void drawEnd(Graphics2D g2d) {
+        g2d.drawOval((int) endPoint.getX(), (int) endPoint.getY(), 10, 10);
     }
-    public boolean checkCollision(Player p, AbstractObstacle[] obstacles) { 
-        return false;  
+
+    public boolean checkCollision(Player p, AbstractObstacle[] obstacles) {
+        return false;
     }
+
     /**
      * draws all components of the level
-     * @param g the drawing utensil 
+     *
+     * @param g the drawing utensil
      */
-    public void showLevel(Graphics2D g) { 
-        drawStart(g); 
-        drawEnd(g); 
-        for (int i = 0; i < obstacles.length; i++) {
-            obstacles[i].draw(g); 
+    
+    //CODE TO TEST AND IMPLEMENT
+    /*
+Plotting Graph of a Function
+package test;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Test extends JPanel {
+
+    private int width = 800;
+    private int heigth = 400;
+    private int padding = 25;
+    private int labelPadding = 25;
+    private Color lineColor = new Color(44, 102, 230, 180);
+    private Color pointColor = new Color(100, 100, 100, 180);
+    private Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private int pointWidth = 1;
+    private int numberYDivisions = 10;
+    private List<Double> scores;
+    
+    public static double graphPoints(String function, double x){
+            try {
+                Expression e = new ExpressionBuilder(function)
+                .variables("x")
+                .build()
+                .setVariable("x", x);
+                return e.evaluate();
+            } catch (Exception e) {
+                System.out.println("Ops, there is a discontinuity in the rollercoaster. Try again!");
+            }
+            return 0;
         }
-    } 
-    /**
-     * makes an exact copy of a level
-     * @return the copied version of a level 
-     */
-    public Level clone() { 
-        Level l2 = new Level(startPoint, endPoint, obstacles); 
-        return l2;
+
+    public Test(List<Double> scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxScore() - getMinScore());
+
+        List<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < scores.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxScore() - scores.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+        
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (scores.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinScore() + (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        for (int i = 0; i < scores.size(); i++) {
+            if (scores.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        // create x and y axes 
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        }
+    }
+
+//    @Override
+//    public Dimension getPreferredSize() {
+//        return new Dimension(width, heigth);
+//    }
+    private double getMinScore() {
+        double minScore = Double.MAX_VALUE;
+        for (Double score : scores) {
+            minScore = Math.min(minScore, score);
+        }
+        return minScore;
+    }
+
+    private double getMaxScore() {
+        double maxScore = Double.MIN_VALUE;
+        for (Double score : scores) {
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore;
+    }
+
+    public void setScores(List<Double> scores) {
+        this.scores = scores;
+        invalidate();
+        this.repaint();
+    }
+
+    public List<Double> getScores() {
+        return scores;
+    }
+
+    private static void createAndShowGui() {
+        List<Double> scores = new ArrayList<>();
+        int maxDataPoints = 500;
+        double initialx = -10;
+        double finalx = 10;
+        double step = (finalx - initialx)/maxDataPoints;
+        double x = initialx;
+        for (int i = 0; i < maxDataPoints; i++) {
+            scores.add(graphPoints("x^2", x));
+            x += step;
+        }
+        Test mainPanel = new Test(scores);
+        mainPanel.setPreferredSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("Level");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+       frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true); 
+        JButton button = new JButton("Run Function");    
+        button.setBounds(100,100,150, 30);    
+                                //textfield to enter name
+        JTextField textfield= new JTextField();
+        textfield.setBounds(110, 50, 130, 30);
+                                //add to frame
+        frame.add(textfield);
+        frame.add(button);   
+        frame.setLayout(null);   
+        frame.setSize(900,700);
+
+                                                //action listener
+        button.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                       	
+                }          
+      });
+        
+    }
+
+}
+    */
+    
+    public void drawFuncArea(Graphics2D g2d){
+        //createAndShowGui();
     }
     
-    public String toString() { 
-        return "LEVEL:\nStart Point: " + startPoint + "\nEnd Point: " + endPoint + 
-                "\nObstacles: " + obstacles;
+    public void render(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        drawStart(g2d);
+        drawEnd(g2d);
+        for (int i = 0; i < obstacles.length; i++) {
+            obstacles[i].draw(g2d);
+        }
+    }
+
+    /**
+     * makes an exact copy of a level
+     *
+     * @return the copied version of a level
+     */
+    public Level clone() {
+        Level l2 = new Level(startPoint, endPoint, obstacles);
+        return l2;
+    }
+
+    public String toString() {
+        return "LEVEL:\nStart Point: " + startPoint + "\nEnd Point: " + endPoint
+                + "\nObstacles: " + obstacles;
     }
 }
